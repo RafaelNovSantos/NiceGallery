c76822f9b682814f582e36c22dfda03aaa89b266
Rafael <rafaelnov84@gmail.com>
2025-04-25 16:08:09 -0300

    Exportação de relatório via pdf

---
+++ NiceGallery.Front-End/wwwroot/js/pdfExport.js
@@ -0,0 +1,13 @@
+﻿window.exportReportToPdf = (produtos) => {
+    const doc = new jsPDF();
+
+    const headers = [["Nome", "Preço"]];
+    const rows = produtos.map(p => [p.name, "R$ " + p.price.toFixed(2)]);
+
+    doc.autoTable({
+        head: headers,
+        body: rows,
+    });
+
+    doc.save("relatorio.pdf");
+};
